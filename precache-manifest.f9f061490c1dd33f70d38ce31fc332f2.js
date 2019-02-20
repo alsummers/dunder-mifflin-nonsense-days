3d74e40911d9bd2695c79640674754370b2c7889
Allison Summers <allisonsummers44@gmail.com>
2019-02-19 21:10:06 -0500

    Updates

---
+++ precache-manifest.f9f061490c1dd33f70d38ce31fc332f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c1fb45fb2570df93ba09a3d6034370b",
+    "url": "/dunder-mifflin-nonsense-days/static/media/dunder-mifflin-logo-5E325D4D51-seeklogo.com.0c1fb45f.png"
+  },
+  {
+    "revision": "301e2d8c4c7c59a33f50",
+    "url": "/dunder-mifflin-nonsense-days/static/js/runtime~main.301e2d8c.js"
+  },
+  {
+    "revision": "f2c8e9949e248ee570d1",
+    "url": "/dunder-mifflin-nonsense-days/static/js/main.f2c8e994.chunk.js"
+  },
+  {
+    "revision": "d0be77769111ce3a3224",
+    "url": "/dunder-mifflin-nonsense-days/static/js/2.d0be7776.chunk.js"
+  },
+  {
+    "revision": "f2c8e9949e248ee570d1",
+    "url": "/dunder-mifflin-nonsense-days/static/css/main.2ef7a6e8.chunk.css"
+  },
+  {
+    "revision": "52645413dd4a644edf09cfd8f3ed3ec1",
+    "url": "/dunder-mifflin-nonsense-days/index.html"
+  }
+];
